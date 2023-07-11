71dc88242901ec9d59d152f6961b0498199612ab
Hasajapathirana <80383870+Hasajapathirana@users.noreply.github.com>
2023-07-11 20:28:37 +0530

    Update config.sh

--- config.sh
+++ config.sh
@@ -4,7 +4,7 @@
 export FOX_BRANCH="fox_12.1"
 export DT_LINK="https://github.com/Hasajapathirana/android_device_samsung_p3s-1 -b android-12.1"
 
-export DEVICE=""
+export DEVICE="p3s"
 export OEM="samsung"
 
 # Build Target
